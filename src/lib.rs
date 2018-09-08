40fcf890342634b0b562d6b82a623deac744c301
Ilya Lunev <ilunev@protonmail.com>
2018-09-08 22:40:49 +0300

    add `TranslateAPI` struct with implementations for `new` and `make_url` methods

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+extern crate url;
+extern crate reqwest;
+
+use url::Url;
+use std::borrow::Borrow;
+
+
+const BASE_URL: &str = "https://translate.yandex.net/api/v1.5/tr.json";
+
+
+pub struct TranslateAPI {
+    key: String,
+    client: reqwest::Client,
+}
+
+impl TranslateAPI {
+    pub fn new(key: String) -> TranslateAPI {
+        let client = reqwest::Client::new();
+        TranslateAPI {
+            key,
+            client,
+        }
+    }
+
+    fn make_url<I, K, V>(&self, method: &str, params: I) -> Url
+        where I: IntoIterator,
+              I::Item: Borrow<(K, V)>,
+              K: AsRef<str>,
+              V: AsRef<str>,
+    {
+        let mut url = Url::parse(BASE_URL).unwrap();
+        url.path_segments_mut()
+            .unwrap()
+            .push(method);
+        url.query_pairs_mut()
+            .append_pair("key", &self.key)
+            .extend_pairs(params);
+        url
+    }
+}
