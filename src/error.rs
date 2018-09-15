44defdc8a9e6248144f39d400e184ea4cea113be
Ilya Lunev <ilunev@protonmail.com>
2018-09-15 23:18:36 +0300

    remove string field from `UnexpectedResponse`

--- src/error.rs
+++ src/error.rs
@@ -1,24 +1,13 @@
-use std::io::Read;
-
-
+#[derive(Debug)]
 pub enum Error {
     ApiError(ApiError),
-    UnexpectedResponse(String),
+    UnexpectedResponse,
     RequestFailed,
 }
 
-impl Error {
-    pub fn unexpected_from_reader<R>(reader: &mut R) -> Error
-        where R: Read,
-    {
-        let mut content = String::new();
-        let _ = reader.read_to_string(&mut content);            
-        Error::UnexpectedResponse(content)
-    }
-}
 
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct ApiError {
     pub code: u16,
     pub message: String,
@@ -31,7 +20,6 @@ pub struct ApiError {
 mod tests {
     use super::*;
     use serde_json;
-    use std::io::{Cursor};
 
     #[test]
     fn deserialize_error_response() {
@@ -42,16 +30,5 @@ mod tests {
         let parsed: ApiError = serde_json::from_str(json).unwrap();
         assert_eq!(401, parsed.code);
         assert_eq!("API key is invalid", parsed.message);
-    }
-
-    #[test]
-    fn unexpected_response_constructor() {
-        let mut file = Cursor::new("error".to_string());
-        let error = Error::unexpected_from_reader(&mut file);
-        if let Error::UnexpectedResponse(content) = error {
-            assert_eq!("error".to_string(), content);
-        } else {
-            panic!("Wrong enum variant");
-        }
     }
 }
